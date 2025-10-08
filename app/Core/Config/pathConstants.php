518ca2effddecb1d02fae9c2eab858b67b512674
Petar Utiliter <petar@utiliter.com>
2025-10-08 09:32:38 +0200

    add path constants

--- app/Core/Config/pathConstants.php
+++ app/Core/Config/pathConstants.php
@@ -3,7 +3,8 @@
 
 
 
-
+const LOGS = __DIR__ . "/../../../logs/";
 const EMAIL_LOGS = __DIR__ . "/../../../logs/mail_logs/";
+const EMAIL_ERROR_LOGS = __DIR__ . "/../../../logs/email_error_logs/";
 
 ?>
