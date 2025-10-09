3ca1b85b5842bd65679d85352d29d53c085ada53
Petar Utiliter <petar@utiliter.com>
2025-10-09 09:44:58 +0200

    routes - add log operation name

--- app/modules/users/routes.php
+++ app/modules/users/routes.php
@@ -7,32 +7,28 @@ use App\Modules\Users\Controllers\UserController;
 
 
 // USER
-$router->get(UserController::class, "list", false);
+$router->get(UserController::class, "list", "user_list", false);
+
+$router->get(UserController::class, "get_user_roles", "user_roles_get", true);
+$router->get(UserController::class, "get_user_types", "user_types_get", true);
 
 
-$router->get(UserController::class, "get_user_roles", true);
-$router->get(UserController::class, "get_user_types", true);
+$router->post(UserController::class, "change_password", "user_change_password", true);
 
+$router->post(UserController::class, "edit_account", "user_edit", true);
 
-$router->post(UserController::class, "change_password", true);
+$router->post(UserController::class, "create_account", "user_create", false);
 
-
-$router->post(UserController::class, "edit_account", true);
-
-$router->post(UserController::class, "create_account", false);
-
-$router->get(UserController::class, "delete_account", false);
-$router->get(UserController::class, "send_verify_delete_token", true);
+$router->get(UserController::class, "delete_account", "user_deletion", false);
+$router->get(UserController::class, "send_verify_delete_token", "user_send_delete_token", true);
 
 // REGISTER
-$router->post(RegisterController::class, "register", false);
-
-$router->get(RegisterController::class, "verify_account", false);
-
+$router->post(RegisterController::class, "register", "user_registration", false);
+$router->get(RegisterController::class, "verify_account", "user_verify_account", false);
 
 // LOGIN
-$router->post(LoginController::class, "login", false);
+$router->post(LoginController::class, "login", "user_login", false);
 
-$router->post(LoginController::class, "logout", true);
+$router->post(LoginController::class, "logout", "user_logout", true);
 
 ?>
