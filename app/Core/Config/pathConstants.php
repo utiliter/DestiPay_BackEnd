d6c474a21c5a8947f091030b6fa211657b86ab4c
Petar Utiliter <petar@utiliter.com>
2025-10-09 09:37:10 +0200

    path constants new chagnes

--- app/Core/Config/pathConstants.php
+++ app/Core/Config/pathConstants.php
@@ -4,6 +4,8 @@
 
 
 const LOGS = __DIR__ . "/../../../logs/";
+const CACHE = __DIR__ . "/../../../cache/";
+const OPERATION_TYPES_CACHE = __DIR__ . "/../../../cache/operation_types.php";
 const EMAIL_LOGS = __DIR__ . "/../../../logs/mail_logs/";
 const EMAIL_ERROR_LOGS = __DIR__ . "/../../../logs/email_error_logs/";
 
