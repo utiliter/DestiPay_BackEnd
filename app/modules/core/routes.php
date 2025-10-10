0af0ed5fa800ef8647b2bf30b4222e4d77a1f2a2
Petar Utiliter <petar@utiliter.com>
2025-10-10 13:37:02 +0200

    language route - token changes

--- app/modules/core/routes.php
+++ app/modules/core/routes.php
@@ -2,7 +2,7 @@
 
 use App\Modules\Core\Controllers\SettingsController;
 
-$router->get(SettingsController::class, "get_languages", "settings_get_language", true);
+$router->get(SettingsController::class, "get_languages", "settings_get_language", false);
 
 
 
